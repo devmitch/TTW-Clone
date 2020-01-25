265921bc33368c093cd37b3b474c42ad96f06bf1
Vincent Wong <TheBrutux168@gmail.com>
2020-01-25 20:02:54 +1100

    Add more information to History Bar

--- src_static/js/history_bar.js
+++ src_static/js/history_bar.js
@@ -13,22 +13,44 @@ export default class HistoryBar extends React.Component {
             overflowY: "scroll"
         }
         this.tableStyle = {
+            marginTop: "5px",
             borderCollapse: "collapse",
-            width: "100%"
+            width: "95%",
+            backgroundColor: "grey",
+            marginLeft: "auto",
+            marginRight: "auto",
+        }
+        this.cellStyle = this.headingStyle = {
+            border: "1px solid white",
+            textAlign: "left",
         }
 
         this.renderTableData = this.renderTableData.bind(this);
+        this.renderAverages = this.renderAverages.bind(this);
+    }
+
+    renderAverages() {
+        let times = this.props.solves.map(x => x.time);
+        let bestSingle = Math.min(...times);
+
+        return (
+            <tr>
+                <td style={this.cellStyle}>{bestSingle}</td>
+                <td style={this.cellStyle}>To be implemented</td>
+                <td style={this.cellStyle}>To be implemented</td>
+            </tr>
+        )
     }
 
     renderTableData() {
         console.log(this.props.solves)
-        return this.props.solves.map(solve => {
+        return this.props.solves.reverse().map(solve => {
             console.log(solve)
             const {scramble, time} = solve
             return (
                <tr>
-                  <td>{scramble}</td>
-                  <td>{time}</td>
+                  <td style={this.cellStyle}>{scramble}</td>
+                  <td style={this.cellStyle}>{time}</td>
                </tr>
             )
         })
@@ -39,9 +61,18 @@ export default class HistoryBar extends React.Component {
             <div style={this.divStyle}>
                 <h1>Solve History</h1>
                 <table style={this.tableStyle}>
+                    <tr>
+                        <th style={this.headingStyle}>Best Single</th>
+                        <th style={this.headingStyle}>Best Ao5</th>
+                        <th style={this.headingStyle}>Current Ao5</th>
+                    </tr>
+                    {this.renderAverages()}
+                </table>
+
+                <table style={this.tableStyle}>
                 <tr>
-                    <th>Time</th>
-                    <th>Scramble</th>
+                    <th style={this.headingStyle}>Time</th>
+                    <th style={this.headingStyle}>Scramble</th>
                 </tr>
                 {this.renderTableData()}
                 </table>
